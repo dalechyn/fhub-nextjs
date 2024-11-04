ae6134193a2da4134a09c54c50630514ce842340
dalechyn <vlad.dalechin@gmail.com>
2024-11-04 15:02:15 +0200

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Card, CardContent } from "@/components/ui/card";
-import { useGetCastSuspenseQuery } from "@/lib/query/useGetCastSuspenseQuery";
+// import { useGetCastSuspenseQuery } from "@/lib/query/useGetCastSuspenseQuery";
 import { Actions } from "fhub";
 import { Types } from "fhub/Node";
 import { useEffect, useState } from "react";
@@ -39,7 +39,7 @@ return <Card>
 }
 
 export default function Home() {
-  const getCastSuspenseQuery = useGetCastSuspenseQuery({fid: 11517n, hash: '0x404d509c220ea59dcada18ad8496f8ef115db1d2'})
+  // const getCastSuspenseQuery = useGetCastSuspenseQuery({fid: 11517n, hash: '0x404d509c220ea59dcada18ad8496f8ef115db1d2'})
 const [casts, setCasts] = useState<Types.Cast[]>([]);
 
   useEffect( () => {
@@ -57,6 +57,6 @@ const [casts, setCasts] = useState<Types.Cast[]>([]);
   return <>
     <h1 className="text-3xl text-center">provided by Build your own client ™</h1>
     {casts.map((cast, i) => <Cast cast={cast} key={i}/>)}
-    <Cast cast={getCastSuspenseQuery.data}/>
+    {/* <Cast cast={getCastSuspenseQuery.data}/> */}
   </>
 }
